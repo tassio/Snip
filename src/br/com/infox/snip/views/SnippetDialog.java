8cd5be1d0f8c83207599a4f717b003177d12f5ca
Gabriel Morrison <gdantas2@gmail.com>
2011-11-03 10:05:29 -0300

    Diálogo pronto para o master

--- src/br/com/infox/snip/views/SnippetDialog.java
+++ src/br/com/infox/snip/views/SnippetDialog.java
@@ -1,7 +1,5 @@
 package br.com.infox.snip.views;
 
-import java.sql.SQLException;
-
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -12,10 +10,10 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-import br.com.infox.snip.dao.SnippetDao;
 import br.com.infox.snip.models.Snippet;
 
 public class SnippetDialog extends Dialog {
@@ -46,27 +44,56 @@ public class SnippetDialog extends Dialog {
 	
 	private void createContents(final Shell parent) {
 		parent.setLayout(new FormLayout());
+		
+		Label labelName = new Label(parent, SWT.LEFT);
+		labelName.setText("Nome");
+		FormData labelNameData = new FormData();
+		labelNameData.left = new FormAttachment(0, 5);
+		labelNameData.top = new FormAttachment(0, 5);
+		labelName.setLayoutData(labelNameData);
+		
 		txtName = new Text(parent, SWT.SINGLE | SWT.BORDER);
-		txtName.setText(snippet.getName());
+		FormData nameData = new FormData(350, 20);
+		nameData.left = new FormAttachment(labelName, 5);
+		txtName.setLayoutData(nameData);
+		
+		Label labelSnippet = new Label(parent, SWT.LEFT);
+		labelSnippet.setText("Snippet");
+		FormData labelSnippetData = new FormData();
+		labelSnippetData.left = new FormAttachment(0, 5);
+		labelSnippetData.top = new FormAttachment(txtName, 10);
+		labelSnippet.setLayoutData(labelSnippetData);
+		
+		Text txtSnippet = new Text(parent, SWT.MULTI | SWT.BORDER);
+		FormData snippetData = new FormData(750, 500);
+		snippetData.left = new FormAttachment(0, 15);
+		snippetData.top = new FormAttachment(labelSnippet, 7);
+		txtSnippet.setLayoutData(snippetData);
+		
+		Button cancelButton = new Button(parent, SWT.PUSH);
+		cancelButton.setText("Cancelar");
+		cancelButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				parent.close();
+			}
+		});
+		FormData cancelData = new FormData(80, 30);
+		cancelData.right = new FormAttachment(98);
+		cancelData.bottom = new FormAttachment(95);
+		cancelButton.setLayoutData(cancelData);
 		
 		Button saveButton = new Button(parent, SWT.PUSH);
 		saveButton.setText("Salvar");
 		saveButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				SnippetDao dao = new SnippetDao();
-				snippet.setName(txtName.getText());
-				try {
-					dao.update(snippet);
-				} catch (SQLException e1) {
-					MessageDialog.openError(parent, "Snip View", e1.getMessage());
-				}
+				MessageDialog.openInformation(parent, "Teste", "OK");
 			}
 		});
-		
 		FormData saveData = new FormData(80, 30);
-		saveData.right = new FormAttachment(98);
-		saveData.bottom = new FormAttachment(95);
+		saveData.right = new FormAttachment(cancelButton, -5, SWT.LEFT);
+		saveData.bottom = new FormAttachment(cancelButton, 0, SWT.BOTTOM);
 		saveButton.setLayoutData(saveData);
 	}
 }
